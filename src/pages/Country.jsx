df3229feb40f00ce3187c9756c1803b06e95ac6c
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-07-28 09:57:24 +0100

    lyudmyla

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,36 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 const Country = () => {
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    const gatData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    gatData();
+  }, [countryId]);
+  console.log(country);
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {loading && <Loader />}
+        {error && <Heading title="Something went wrong" bottom />}
+        {country && <CountryInfo {...country} />}
       </Container>
     </Section>
   );
